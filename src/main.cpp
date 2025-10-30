fa4b6265cfe35b800439cb12624678fa0051a442
Nirolot <federicopirola09@gmail.com>
2025-10-30 16:27:35 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,13 @@
 class CpuPaddle : public Paddle
 {
 public:
-    void Update(int ball_y)
+    void Update()
     {
-        if (y + height / 2 > ball_y)
+        if (IsKeyDown(KEY_W))
         {
             y -= speed;
         }
-        else if (y + height / 2 <= ball_y)
+        else if (IsKeyDown(KEY_S))
         {
             y += speed;
         }
@@ -21,9 +21,9 @@ public:
 };
 
 Ball ball;
-Paddle player;
+Paddle p1;
 CpuPaddle cpu;
-Paddle player2;
+CpuPaddle p2;
 
 int main()
 {
@@ -36,12 +36,12 @@ int main()
     ball.speed_x = 11;
     ball.speed_y = 11;
 
-    player.width = 25;
-    player.height = 120;
-    player.x = screenWidth - player.width - 10;
-    player.y = screenHeight / 2 - player.height / 2;
-    player.speed = 10;
-    player.score = 0;
+    p1.width = 25;
+    p1.height = 120;
+    p1.x = screenWidth - p1.width - 10;
+    p1.y = screenHeight / 2 - p1.height / 2;
+    p1.speed = 10;
+    p1.score = 0;
 
     cpu.width = 25;
     cpu.height = 120;
@@ -50,7 +50,7 @@ int main()
     cpu.speed = 10;
     cpu.score = 0;
 
-    player2 = cpu;
+    p2 = cpu;
 
     bool Title = true;
 
@@ -59,18 +59,24 @@ int main()
         if (Title)
         {
             char *testo = "Pong";
+            char *testo2 = "Press enter to play";
             int fontSize = 120;
+            int fontSize2 = 50;
             int larghezzaTesto = MeasureText(testo, fontSize);
+            int larghezzaTesto2 = MeasureText(testo2, fontSize2);
 
             int posX = (screenWidth / 2) - (larghezzaTesto / 2);
             int posY = screenHeight / 4;
+            int posX2 = (screenWidth / 2) - (larghezzaTesto2 / 2);
+            int posY2 = screenHeight / 2 + fontSize2 * 2;
 
             BeginDrawing();
             ClearBackground(BLACK);
-            DrawRectangle(screenWidth / 3, screenHeight / 2 + 40, 100, 50, WHITE);
-            DrawRectangle(screenWidth / (4.0 / 3.0) - 200, screenHeight / 2 + 40, 100, 50, WHITE);
+            // DrawRectangle(screenWidth / 3, screenHeight / 2 + 40, 100, 50, WHITE);
+            // DrawRectangle(screenWidth / (4.0 / 3.0) - 200, screenHeight / 2 + 40, 100, 50, WHITE);
             DrawText(testo, posX, posY, fontSize, WHITE);
-            DrawText("P v P", screenWidth / 3, screenHeight / 2 + 40, 30, BLACK);
+            DrawText(testo2, posX2, posY2, fontSize2, WHITE);
+            // DrawText("P v P", screenWidth / 3, screenHeight / 2 + 40, 30, BLACK);
             EndDrawing();
 
             if (IsKeyDown(KEY_ENTER))
@@ -81,15 +87,15 @@ int main()
 
         else
         {
-            ball.Update(cpu.score, player.score);
-            player.Update();
-            cpu.Update(ball.y);
+            ball.Update(p2.score, p1.score);
+            p1.Update();
+            p2.Update();
 
-            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
+            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{p1.x, p1.y, p1.width, p1.height}))
             {
                 ball.speed_x *= -1;
             }
-            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
+            if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{p2.x, p2.y, p2.width, p2.height}))
             {
                 ball.speed_x *= -1;
             }
@@ -97,12 +103,12 @@ int main()
             BeginDrawing();
             ClearBackground(BLACK);
             DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
-            player.Draw();
-            cpu.Draw();
+            p1.Draw();
+            p2.Draw();
             ball.Draw();
 
-            DrawText(TextFormat("%i", cpu.score), screenWidth / 4 - 20, 20, 80, WHITE);
-            DrawText(TextFormat("%i", player.score), screenWidth / 4 * 3 - 20, 20, 80, WHITE);
+            DrawText(TextFormat("%i", p2.score), screenWidth / 4 - 20, 20, 80, WHITE);
+            DrawText(TextFormat("%i", p1.score), screenWidth / 4 * 3 - 20, 20, 80, WHITE);
             EndDrawing();
         }
     }
